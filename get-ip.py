8b78c87fc3aefa39825fdbf1b060856b243e327f
Jason Ma <crusader.jie@gmail.com>
2019-05-04 23:29:59 +0800

    second commit

--- get-ip.py
+++ get-ip.py
@@ -8,4 +8,5 @@ url = "http://checkip.dyndns.org"
 with urllib.request.urlopen(url) as response:
     html = response.read().decode("utf-8")
 theIP = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", html)
+
 print("your IP Address is: {}".format(theIP))
